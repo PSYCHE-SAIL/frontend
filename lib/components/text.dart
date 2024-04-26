fbba8d7897440493fe02be01786b5e89c128d23a
DISHA DWIVEDI <dishadwivedi123g@gmai.com>
2024-04-27 01:32:29 +0530

    history_page1

--- lib/components/text.dart
+++ lib/components/text.dart
@@ -1,14 +1,9 @@
-import 'dart:convert';
-import 'dart:math';
-import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:psychesail/model/time.dart';
 import 'package:random_avatar/random_avatar.dart';
 import './button.dart';
 import 'package:animations/animations.dart';
-
 Widget HigthlightText(fontsize, minheight, txt) {
   return Container(
     constraints: BoxConstraints(
@@ -288,8 +283,7 @@ Widget _maptextbubble(size) {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  constraints: BoxConstraints(
-                      minHeight: size.height / 3, minWidth: size.width / 4),
+                  constraints: BoxConstraints(minHeight: size.height/3 , minWidth: size.width/4),
                   decoration: BoxDecoration(
                       image: DecorationImage(
                     image: AssetImage("assets/maps_image.png"),
@@ -316,225 +310,158 @@ Widget _maptextbubble(size) {
   );
 }
 
-Widget communityContainer(
-    sizeWidth, sizeHeight, constr, heading, description, imagestring) {
+Widget communityContainer(sizeWidth,sizeHeight,constr,heading,description,imagestring) {
   return Container(
-    constraints: BoxConstraints(maxWidth: sizeWidth * 0.8),
-    decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.grey.shade300,
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(8.0))),
-    child: Padding(
-      padding: EdgeInsets.all(sizeHeight * sizeWidth * 0.00004),
-      child: Column(
-        children: [
-          circleButton(constr, sizeWidth / 100, sizeWidth / 50, imagestring),
-          Text(
-            heading,
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: sizeWidth * sizeHeight * 0.00008,
-                fontWeight: FontWeight.bold),
-          ),
-          Text(
-            description,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: sizeWidth * sizeHeight * 0.00004,
-            ),
-            textAlign: TextAlign.center,
-          )
-        ],
-      ),
-    ),
-  );
+                                              constraints: BoxConstraints(
+                                                maxWidth: sizeWidth * 0.5
+                                              ),
+                                              decoration: BoxDecoration(
+                                                border: Border.all(
+                                                  color: Colors.grey.shade300,
+                                                ),
+                                                borderRadius: BorderRadius.all(Radius.circular(8.0))
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsets.all(sizeHeight * sizeWidth * 0.00005),
+                                                child: Column(
+                                                  children: [
+                                                    circleButton(
+                                                              constr, sizeWidth / 100, sizeWidth / 50, imagestring),
+                                                              Text(heading,style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: sizeWidth * sizeHeight * 0.00008,
+                                                    fontWeight: FontWeight.bold
+                                                  ),),
+                                                  Text(description,style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: sizeWidth * sizeHeight * 0.00005,
+                                                    
+                                                  ),
+                                                  textAlign: TextAlign.center,)
+                                                  ],
+                                                ),
+                                              ),
+                                            );
+}
+Widget activityContainer(sizeWidth,sizeHeight,constr,heading,imagestring) {
+  return Container(
+                                              constraints: BoxConstraints(
+                                                maxWidth: sizeWidth * 0.5
+                                              ),
+                                              decoration: BoxDecoration(
+                                               
+                                                borderRadius: BorderRadius.all(Radius.circular(8.0))
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsets.all(sizeHeight * sizeWidth * 0.00005),
+                                                child: Column(
+                                                  children: [
+                                                    circleButton(
+                                                              constr, sizeWidth / 120, sizeWidth / 100, imagestring),
+                                                              Text(heading,style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: sizeWidth * sizeHeight * 0.00005,
+                                                    fontWeight: FontWeight.bold
+                                                  ),),
+                                      
+                                                  ],
+                                                ),
+                                              ),
+                                            );
 }
 
-Widget historyContainer(
-    sizeWidth, sizeHeight, constr, date, time, stressEmoji) {
+Widget historyContainer(sizeWidth,sizeHeight,constr,date,time,stressEmoji) {
   return Container(
-    constraints: BoxConstraints(minWidth: sizeWidth * 0.7),
-    decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.grey.shade300,
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(8.0))),
-    child: Padding(
-      padding: EdgeInsets.all(sizeHeight * sizeWidth * 0.00005),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(Icons.calendar_month_rounded,
-                  color: Color.fromRGBO(35, 154, 139, 75)),
-              SizedBox(width: sizeWidth * 0.02),
-              Text(
-                date,
-                style: TextStyle(
-                    color: Colors.black,
+                                              constraints: BoxConstraints(
+                                                minWidth: sizeWidth * 0.7
+                                              ),
+                                              decoration: BoxDecoration(
+                                                border: Border.all(
+                                                  color: Colors.grey.shade300,
+                                                ),
+                                                borderRadius: BorderRadius.all(Radius.circular(8.0))
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsets.all(sizeHeight * sizeWidth * 0.00005),
+                                                child: Column(
+                                                  mainAxisAlignment: MainAxisAlignment.start,
+                                                  crossAxisAlignment: CrossAxisAlignment.start, 
+                                                  children: [
+                                                    Row(
+                                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                      children: [
+                                                        Column(
+                                                          mainAxisAlignment: MainAxisAlignment.start,
+                                                  crossAxisAlignment: CrossAxisAlignment.start, 
+                                                          children: [
+                                                            Row(
+                                                              mainAxisAlignment: MainAxisAlignment.start,children: [ 
+                                                              Icon(Icons.calendar_month_rounded,color: Color.fromRGBO(35, 154, 139, 75)),
+                                                               SizedBox(width: sizeWidth * 0.02),
+                                                              Text(date,style: TextStyle(
+                                                            color: Colors.black,
+                                                            fontSize: sizeWidth * sizeHeight * 0.000055,
+                                                            fontWeight: FontWeight.bold
+                                                                                                              ),)
+                                                            ],),
+                                                            SizedBox(height: sizeHeight * 0.01),
+                                                         Row(
+                                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                                          children: [ 
+                                                          Icon(Icons.schedule,color: Color.fromRGBO(35, 154, 139, 75)),
+                                                          SizedBox(width: sizeWidth * 0.02),
+                                                          Text(time,style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: sizeWidth * sizeHeight * 0.000055,
+                                                        
+                                                                                                          ),)
+                                                        ],),
+                                                        
+                                                          ],
+                                                        ),
+                                                        if(constr) SizedBox(width : sizeWidth * 0.05),
+                                                        circleButton(
+                                                              constr, sizeWidth / 100, sizeWidth / 50, stressEmoji[0]),
+                                                      ],
+                                                    ),
+                                                    
+                                                    SizedBox(height: sizeHeight * 0.01),
+                                                              Row(
+                                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                                children: [
+              SizedBox(width: sizeWidth * 0.15),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 5000),
+                curve: Curves.linear,
+                child: Text(
+                  stressEmoji[1],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: stressEmoji[2],
                     fontSize: sizeWidth * sizeHeight * 0.000055,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          SizedBox(height: sizeHeight * 0.01),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(Icons.schedule, color: Color.fromRGBO(35, 154, 139, 75)),
-              SizedBox(width: sizeWidth * 0.02),
-              Text(
-                time,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: sizeWidth * sizeHeight * 0.000055,
-                ),
-              )
-            ],
-          ),
-          SizedBox(height: sizeHeight * 0.01),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(width: sizeWidth * 0.15),
-              Text(
-                "Summary of last chat",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: sizeWidth * sizeHeight * 0.000055,
+                  ),
                 ),
               ),
               SizedBox(width: sizeWidth * 0.15),
             ],
-          ),
-          SizedBox(height: sizeHeight * 0.01),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(width: sizeWidth * 0.6),
-              Text(
-                "More",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: sizeWidth * sizeHeight * 0.00005,
-                    fontWeight: FontWeight.bold),
-              ),
-              Icon(Icons.arrow_forward_rounded,
-                  color: Colors.black, size: sizeWidth * sizeHeight * 0.00005)
-            ],
-          )
-        ],
-      ),
-    ),
-  );
-}
-
-Widget activityContainer(context, sizeWidth, sizeHeight, constr, heading,
-    imagestring, pos, currentUserId) {
-  print(pos);
-  var response;
-  return GestureDetector(
-    onTap: () async {
-      List<String> places = ["hospital"];
-      if (heading == "Movies") {
-        places = ["movie_theater"];
-      }
-      if (heading == "Games") {
-        places = ["amusement_park", "amusement_center"];
-      }
-      if (heading == "Cafe") {
-        places = ["cafe", "restaurant"];
-      }
-      // print(hello)
-      response = await searchNearbyPlaces(places, pos);
-      // print("take me to hell");
-      print(response);
-      Navigator.pushNamed(context, '/activity-maps', arguments: {
-        'places': response,
-        'imagestring': imagestring,
-        'currentUserId': currentUserId
-      });
-    },
-    child: Container(
-      constraints: BoxConstraints(maxWidth: sizeWidth * 0.5),
-      decoration:
-          BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-      child: Padding(
-        padding: EdgeInsets.all(sizeHeight * sizeWidth * 0.00005),
-        child: Column(
-          children: [
-            circleButton(constr, sizeWidth / 120, sizeWidth / 100, imagestring),
-            Text(
-              heading,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: sizeWidth * sizeHeight * 0.00005,
-                  fontWeight: FontWeight.bold),
-            ),
-            // activitymaps(sizeWidth, sizeHeight,constr, response, imagestring,currentUserId),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-dynamic searchNearbyPlaces(List<String> places_type, pos) async {
-  var responseData;
-  print(pos);
-  // Define the request body as a JSON object
-  var requestBody = {
-    "includedTypes": places_type,
-    "maxResultCount": 5,
-    "locationRestriction": {
-      "circle": {
-        "center": {"latitude": pos[1], "longitude": pos[0]},
-        "radius": 2000.0
-      }
-    }
-  };
-  print("hellooo");
-  // Encode the request body to JSON
-  var requestBodyJson = jsonEncode(requestBody);
-  print("hiii");
-  // Define the headers
-  var headers = {
-    'Content-Type': 'application/json',
-    'X-Goog-Api-Key':
-        'AIzaSyC1ksDmMNde1jArPaZF1VK-Xad2yFyjjHk', // Replace 'YOUR_API_KEY' with your actual API key
-    'X-Goog-FieldMask': 'places.displayName,places.googleMapsUri'
-  };
-// print("hell");
-  try {
-    // Make the HTTP POST request
-    var response = await http.post(
-      Uri.parse('https://places.googleapis.com/v1/places:searchNearby'),
-      headers: headers,
-      body: requestBodyJson,
-    );
-
-    // Check if the request was successful (status code 200)
-    if (response.statusCode == 200) {
-      // Parse the response body (assuming it's JSON) into a Map
-      responseData = jsonDecode(response.body);
-      // Process the responseData here
-      print(responseData);
-
-      // return responseData;
-    } else {
-      // Request was not successful
-      print('Request failed with status: ${response.statusCode}');
-      print('Response body: ${response.body}');
-      responseData = jsonDecode(response.body);
-    }
-  } catch (e) {
-    // Handle any errors that occurred during the HTTP request
-    print('Error occurred: $e');
-  }
-  return responseData;
+                                                              ),             
+                                                  SizedBox(height: sizeHeight * 0.01),
+                                                              Row(
+                                                                mainAxisAlignment: MainAxisAlignment.end,
+                                                                children: [
+                                                                  SizedBox(width: sizeWidth * 0.6),
+                                                                  Text("More",style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: sizeWidth * sizeHeight * 0.00005,
+                                                    fontWeight: FontWeight.bold
+                                                  ),),
+                                                  SizedBox(width: sizeWidth * 0.006),
+                                                                  Icon(Icons.arrow_forward_rounded,color: Colors.black,size: sizeWidth* sizeHeight * 0.00005)
+                                                                ],
+                                                              )
+                                      
+                                                  ],
+                                                ),
+                                              ),
+                                            );
 }
